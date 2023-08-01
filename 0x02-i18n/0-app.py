b77577bb9beb8b3beb97d7f51b2c1f0684e5360e
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-08-01 15:16:07 +0200

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env pyhton3
+""" 
+flask app
+"""
+from flask import Flask, render_template
 
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+  """
+  Handles / route
+  """
+  return render_template('0-index.html')
+
+if __name__ == "___main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
